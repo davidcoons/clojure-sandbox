7b34022bb2ce119e7d4c57cde5286afa5b9eed80
davidcoons <dac-github@optonline.net>
2013-04-19 13:21:37 -0400

    Created lein project

---
+++ src/clojure_sandbox/core.clj
@@ -0,0 +1,9 @@
+(ns clojure-sandbox.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  ;; work around dangerous default behaviour in Clojure
+  (alter-var-root #'*read-eval* (constantly false))
+  (println "Hello, World!"))
